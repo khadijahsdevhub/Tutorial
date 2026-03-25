8f7859a9abe4830f6d6100a502b7dd907caeb0d9
Olaniyi Opeyemi Khadijah <129620484+khadijahsdevhub@users.noreply.github.com>
2026-03-25 22:17:20 +0100

    commit changes

---
+++ Tutorial/EnumTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+
+    public enum Days:byte
+    {
+        Monday,Tuesday, Wednesday, Thursday, Friday,
+    }
+    internal class EnumTest
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello World");
+
+            Days day1 = 0;
+            Console.WriteLine( day1);
+
+            Days day2 = (Days)2;
+            Console.WriteLine(day2);
+
+            Days day3 = Days.Wednesday;
+            Console.WriteLine((int)day3);
+
+            foreach(byte b in Enum.GetValues(typeof(Days))) 
+                Console.WriteLine($"{b} : {(Days)b}");
+            Console.ReadLine();
+        }
+    }
+}
